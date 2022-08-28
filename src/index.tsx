050b51ce16f79a3538ceba6c0b976a4bcb18fcca
Ahmet CAN <email.ahmetcan@gmail.com>
2022-08-29 01:25:43 +0300

    Add missing index.tsx file

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './tailwind.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+const container = document.getElementById('app');
+const root = createRoot(container!);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
